72128d82bf07b96887c254782708c84fd34877bb
Majdi <chaouachimajdi@gmail.com>
2019-11-08 13:36:47 +0000

    added all files

---
+++ src/app/shared/okta/okta.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+declare let OktaSignIn: any;
+
+@Injectable()
+export class Okta {
+  widget;
+
+  constructor() {
+    this.widget = new OktaSignIn({
+      baseUrl: 'https://publicissapientswaroop.okta.com',
+      clientId: '0oa1r8ica1nyCJrfV357',
+      redirectUri: 'http://localhost:4200',
+      authParams: {
+        responseType: ['id_token', 'token']
+      }
+    });
+  }
+
+  getWidget() {
+    return this.widget;
+  }
+}
